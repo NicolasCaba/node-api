3a3416109628631c5d72bd00f552f91b3bf5d311
Nicolas Caballero <nico_caballero2000@hotmail.com>
2022-04-04 02:43:35 -0500

    mongodb database connection

---
+++ config/mongo.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const dbConnect = () => {
+  const DB_URI = process.env.DB_URI;
+
+  mongoose.connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }, (error, response) => {
+    !error ? console.log('DB connected'): console.log(error);
+  });
+}
+
+module.exports.dbConnect = dbConnect;
