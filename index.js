a6e365b05676d83448402a6f86562741f071d65c
Nicolás Caballero <67204635+NicolasCaba@users.noreply.github.com>
2022-04-07 16:18:16 -0500

    slack error logger

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const dbConnect = require('./config/mongo');
+const loggerStream = require('./utils/handleLogger');
+const morganBody = require('morgan-body');
 
 const app = express();
 
@@ -10,6 +12,15 @@ app.use(express.json());
 
 // Static assets
 app.use(express.static('storage'));
+
+// Morgan config
+morganBody(app, {
+  noColors: true,
+  stream: loggerStream,
+  skip: function (req, res) {
+    return res.statusCode < 400;
+  }
+});
 
 // Routes
 app.use('/api', require('./routes/index.routes'));
